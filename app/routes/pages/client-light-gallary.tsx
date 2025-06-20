6a4ba9d3fbf3e11bc155e0bc36476f6533ffb483
Ashraf Elshaer <ash@anterasoftware.com>
2025-06-20 14:32:21 -0500

    wip

--- app/routes/pages/client-light-gallary.tsx
+++ app/routes/pages/client-light-gallary.tsx
@@ -1,30 +1,51 @@
+import { Typography } from "@mui/material";
 import { LightgalleryProvider, LightgalleryItem } from "react-lightgallery";
 
 
 export const clientLoader = async () => {
   return {
     images: [
+      "https://picsum.photos/200",
       "https://picsum.photos/200/300",
       "https://picsum.photos/200/300",
-      "https://picsum.photos/200/300",
+      "https://picsum.photos/400/300",
+      "https://picsum.photos/200/600",
     ],
   };
 };
 
 export default function ServerLightGallary({ loaderData }: { loaderData: { images: string[] } }) {
+
+  
   return (
     <div>
-      <LightgalleryProvider>
+      <Typography variant="h3">Multiple Images</Typography>
+      <LightgalleryProvider
+      className="grid grid-cols-3 gap-4"
+      >
+        <div className="grid grid-cols-3 gap-4">
+
         {loaderData.images.map((image, index) => (
           <LightgalleryItem
-            key={image + index}
-            galleryClassName=" "
-            group="any"
-            src={image}
+          key={image + index}
+          galleryClassName=" "
+          group="any"
+          src={image}
           >
             <img src={image} alt="image" />
           </LightgalleryItem>
         ))}
+        </div>
+      </LightgalleryProvider>
+
+
+      <Typography variant="h3">Single Image</Typography>
+      <LightgalleryProvider>
+        <LightgalleryItem
+          src={loaderData.images[0]}
+        >
+          <img src={loaderData.images[0]} alt="image" />
+        </LightgalleryItem>
       </LightgalleryProvider>
     </div>
   );
