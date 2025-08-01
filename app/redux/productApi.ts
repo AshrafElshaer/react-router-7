842d6f037cb6a3f2eed688424c2f11dddc53aaa1
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2025-07-31 20:59:23 -0500

    wip

--- app/redux/productApi.ts
+++ app/redux/productApi.ts
@@ -3,20 +3,22 @@ import {
   coreModule,
   reactHooksModule,
   fetchBaseQuery,
-
 } from "@reduxjs/toolkit/query/react";
-
 
 const createApi = buildCreateApi(
   coreModule(),
-  reactHooksModule({ unstable__sideEffectsInRender: true }),
+  reactHooksModule({ unstable__sideEffectsInRender: false }) // Change this to false for SSR
 );
 
 export const productApi = createApi({
   reducerPath: "productApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://dev-webstore.aether-stores.io/public/api/v1/" }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: "https://dev-webstore.aether-stores.io/public/api/v1/",
+  }),
   endpoints: (builder) => ({
-    getProduct: builder.query<any, void>({ query: () => "front/products/6607" }),
+    getProduct: builder.query<any, void>({ 
+      query: () => "front/products/6607",
+    }),
   }),
 });
 
