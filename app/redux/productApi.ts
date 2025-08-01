5101cc3718b1d0d3dce6fd146ac054300208f6a5
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2025-07-31 20:16:10 -0500

    cleanup

--- app/redux/productApi.ts
+++ app/redux/productApi.ts
@@ -6,14 +6,6 @@ import {
 
 } from "@reduxjs/toolkit/query/react";
 
-import type { Action, PayloadAction } from '@reduxjs/toolkit'
-import { HYDRATE } from 'next-redux-wrapper'
-
-type RootState = any // normally inferred from state
-
-function isHydrateAction(action: Action): action is PayloadAction<RootState> {
-  return action.type === HYDRATE
-}
 
 const createApi = buildCreateApi(
   coreModule(),
@@ -24,8 +16,8 @@ export const productApi = createApi({
   reducerPath: "productApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://dev-webstore.aether-stores.io/public/api/v1/" }),
   endpoints: (builder) => ({
-    getProducts: builder.query<any, void>({ query: () => "front/products/6607" }),
+    getProduct: builder.query<any, void>({ query: () => "front/products/6607" }),
   }),
 });
 
-export const { useGetProductsQuery } = productApi;
+export const { useGetProductQuery } = productApi;
--- app/routes/pages/rtk-query.tsx
+++ app/routes/pages/rtk-query.tsx
@@ -1,13 +1,13 @@
-import { useGetProductsQuery, productApi } from "../../redux/productApi";
+import { useGetProductQuery, productApi } from "../../redux/productApi";
 import htmlParse from "html-react-parser";
 
 export const loader = async () => {
-  productApi.endpoints.getProducts.initiate();
+  productApi.endpoints.getProduct.initiate();
   return;
 };
 
 export default function RtkQuery() {
-  const { data } = useGetProductsQuery();
+  const { data } = useGetProductQuery();
   return (
     <div>
       {data && <h2>{data.productName}</h2>}
